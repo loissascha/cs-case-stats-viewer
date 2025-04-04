1b9351fdf9eb0a52e29ee3c2455ed0ce4507a740
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-04-04 09:56:16 +0200

    podman

---
+++ extractor/podman.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir -p ./output
+
+podman build -t my-puppeteer-app .
+podman run --rm -it \
+  --env WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
+  --env XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
+  -v $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY \
+  -v "$(pwd)/output:/app/output" \
+  --device /dev/dri \
+  --security-opt seccomp=unconfined \
+  --privileged \
+  my-puppeteer-app
